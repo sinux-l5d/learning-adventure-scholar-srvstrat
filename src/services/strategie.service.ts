f0fca8bc121d50c9adb97549104a849ac4c019ad
Romain Pajean <pajeanromain@gmail.com>
2022-02-23 15:17:04 +0100

    fix: utilisation syntaxe ts
    changement de la boucle for
    et changement du try catch

--- src/services/strategie.service.ts
+++ src/services/strategie.service.ts
@@ -11,25 +11,19 @@ export class StrategieService {
    * @throws Error si l'exercice n'a pas été trouvé
    */
   public static async getIdNextSimpleById(id: ExerciceId['id']): Promise<ExerciceId['id'] | null> {
-    try {
-      // dans le tableau des ID prendre le suivant
-      // pour simplifier envoyer toujours le même suivant
-      // coder en dur ici
-      var listeExo = ['exo1', 'exo2', 'exo3'];
+    // dans le tableau des ID prendre le suivant
+    // pour simplifier envoyer toujours le même suivant
+    // coder en dur ici
+    const listeExo = ['exo1', 'exo2', 'exo3'];
 
-      // si nous ne sommes pas au dernier exercice
-      // il renvoie le suivant
-      for (let i = 0; i < listeExo.length - 1; i++) {
-        if (listeExo[i] == id) {
-          return listeExo[(i + 1) % listeExo.length];
-        }
+    listeExo.forEach((value, index, array) => {
+      if (value === id) {
+        // si out range return null sinon renvoie l'id suivant
+        return listeExo[index + 1] ?? null;
       }
+    });
 
-      // si l'exercice courant est en dernière position ou n'est pas dans la liste, il renvoie null.
-      //TODO: Faut il renvoyer une erreur, "", "fini" ou null ?
-      return null;
-    } catch (err) {
-      throw new Error(`Not found: ${err}`);
-    }
+    //TODO:throw error
+    return null;
   }
 }
